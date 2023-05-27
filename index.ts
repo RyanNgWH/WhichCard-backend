992f2fa7d65fa35621b958a5f677ceaa29a17fc1
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2023-05-28 02:52:33 +0800

    Add new application dependencies
    - Add JSON body parser dependency
    - Add UUID dependency
    - Update entry point to load JSON body parser middleware

--- index.ts
+++ index.ts
@@ -4,11 +4,23 @@
  * @format
  */
 import express from 'express';
+import bodyParser from 'body-parser';
 import v1UserRouter from './src/v1/routes/userRoutes';
 
+/**
+ * Initialize express app and api port
+ */
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+/**
+ * Load JSON body parser middleware
+ */
+app.use(bodyParser.json());
+
+/**
+ * User management routes
+ */
 app.use('/api/v1/users', v1UserRouter);
 
 app.listen(PORT, () => {
