dbb2702c5e96b84efae40fbfded55a395c8bfd47
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2023-05-27 21:03:01 +0800

    Add versioning for the api
    - Add version 1 of the app
    - Add route from entry point to v1 of the app

--- index.ts
+++ index.ts
@@ -4,13 +4,12 @@
  * @format
  */
 import express from 'express';
+import router from './src/v1/routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h2>Hello World!</h2>');
-});
+app.use('/api/v1', router);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
