1217bbd1b1beff9a6688a9dcb913cc8e4eb01379
Avosthe <u.cheejingjie@gmail.com>
2023-07-20 16:57:53 +0800

    Add password hashing with SHA-256 algorithm.

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,11 +4,20 @@
  * @format
  */
 
+import crypto from "crypto";
 import { v4 as uuidv4 } from 'uuid';
 import * as cardDatabase from '../database/cardDatabase';
 import * as userDatabase from '../database/userDatabase';
 import { User, UserCardRequest } from '../shared/types';
 import CardNotFoundError from '../shared/errors/database/card/cardNotFoundError';
+
+/**
+ * Hash Password
+ * @returns Hashed password
+ */
+async function hashPassword(str: string) {
+  return crypto.createHash("sha256").update(str).digest("hex");
+}
 
 /**
  * Get all users
@@ -38,9 +47,11 @@ async function createUser(
   newUser: Pick<User, 'name' | 'email' | 'password' | 'cards'>,
 ) {
   // Create new user object with id and timestamps
+  const passwordHash = await hashPassword(newUser.password);
   const userToAdd = {
     ...newUser,
     _id: uuidv4(),
+    password: passwordHash,
     createdAt: new Date().getTime(),
     updatedAt: new Date().getTime(),
   };
@@ -159,7 +170,8 @@ async function deleteUserCardByName(userId: string, cardName: string) {
  * @returns The logged in user, or undefined if user does not exist
  */
 async function login(email: string, password: string) {
-  const user = await userDatabase.login(email, password);
+  const passwordHash = await hashPassword(password);
+  const user = await userDatabase.login(email, passwordHash);
   return user;
 }
 
