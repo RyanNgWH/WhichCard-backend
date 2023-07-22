b7a12d6cb7541adbd351b49b28a4fd182d3ae4b9
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2023-07-22 22:34:46 +0800

    Add test database seeding
    - Add package dependencies
    - Add database seed data for the following collections
      - Cards
      - Merchants
      - Transactions
      - Users

---
+++ tests/data/transactions/transactions.ts
@@ -0,0 +1,25 @@
+/**
+ * Seed data for the transactions collection.
+ *
+ * @format
+ */
+
+const transactions = [
+  {
+    _id: 'b6b325d1-ea0f-470f-9762-e9b632c4e71e',
+    user: '3618ddc6-3c4c-48b3-9dfd-5242b0fbf897',
+    userCard: 'My lovely ocbc',
+    merchant: 'f6c42c4a-e63e-434a-8dca-a99d2aff7bc4',
+    dateTime: {
+      $date: '2023-07-02T00:00:00.000Z',
+    },
+    amount: 1000,
+    cashbackAmount: 60,
+    cashbackCategory: 'dining',
+    createdAt: 1689963842791,
+    updatedAt: 1689963842791,
+    __v: 0,
+  },
+];
+
+export default transactions;
---
+++ tests/data/users/users.ts
@@ -0,0 +1,49 @@
+/**
+ * Seed data for the users collection.
+ *
+ * @format
+ */
+
+const users = [
+  {
+    _id: '3618ddc6-3c4c-48b3-9dfd-5242b0fbf897',
+    name: 'Jang Man Wol',
+    email: 'jmwl160493@kakaot.com',
+    password:
+      'b03ddf3ca2e714a6548e7495e2a03f5e824eaac9837cd7f159c67b90fb4b7342',
+    cards: [
+      {
+        cardName: 'My lovely ocbc',
+        cardExpiry: '2027-05-01T00:00:00.000Z',
+        card: 'dba21fa7-ec07-47d0-9e14-66afe3157829',
+      },
+    ],
+    createdAt: 1689916468269,
+    updatedAt: 1689916809294,
+    __v: 1,
+  },
+  {
+    _id: 'a3cec349-8d87-411e-8430-f3e4e16c8054',
+    name: 'Sunny',
+    email: 'kimsun@goblinchiken.com',
+    password:
+      '263e895c8882006349e41dc4a09b80ee2d8802d4b85218996cc5dee7bf3bc744',
+    cards: [],
+    createdAt: 1689916611983,
+    updatedAt: 1689916611983,
+    __v: 0,
+  },
+  {
+    _id: 'aea01230-7f6a-4e46-acc8-7643f1ea126e',
+    name: 'Sanchez',
+    email: 'sanchez@pizzaalvolo.com',
+    password:
+      '84d615bdedbcec705efc5e5a90a07b914d0a36d0e56185df29022d8fda22dae6',
+    cards: [],
+    createdAt: 1689916672071,
+    updatedAt: 1689916672071,
+    __v: 0,
+  },
+];
+
+export default users;
